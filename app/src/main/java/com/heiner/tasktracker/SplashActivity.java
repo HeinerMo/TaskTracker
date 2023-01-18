39a69263a6c996c8c28c6eb97520d5f3ace40d43
Heiner <heinermongelopez@gmail.com>
2023-01-18 13:47:11 -0600

    Spalsh activity now loads the data of the app at launch

--- app/src/main/java/com/heiner/tasktracker/SplashActivity.java
+++ app/src/main/java/com/heiner/tasktracker/SplashActivity.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
+import com.heiner.tasktracker.data.TaskData;
+
 public class SplashActivity extends AppCompatActivity {
 
     @Override
@@ -15,6 +17,7 @@ public class SplashActivity extends AppCompatActivity {
 
 
         //Load all user data
+        TaskData.getInstance().getTasks();
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
